f092d05d337118f75624c9deedc572eb1395e5a2
BasicCorruption <saytleader@outlook.com>
2023-04-30 09:51:54 -0400

    Update ytembed.js

--- public/javascripts/ytembed.js
+++ public/javascripts/ytembed.js
@@ -7,6 +7,7 @@ function loadVideo(iframe) {
 			? Number(iframe.getAttribute("vnum"))
 			: 0;
 		console.log(videoNumber);
+		console.log(data.items.length);
 		var link = data.items[videoNumber].link;
 		id = link.substr(link.indexOf("=") + 1);
 		iframe.setAttribute(
