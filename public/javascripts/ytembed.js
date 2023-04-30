e0efd0d46e4f5da41f486ce905e9f096dc4b10d2
BasicCorruption <saytleader@outlook.com>
2023-04-30 10:13:37 -0400

    Update ytembed.js

--- public/javascripts/ytembed.js
+++ public/javascripts/ytembed.js
@@ -1,15 +1,13 @@
 var reqURL =
 	"https://api.rss2json.com/v1/api.json?rss_url=" +
-	encodeURIComponent("https://www.youtube.com/feeds/videos.xml?playlist_id=");
+	encodeURIComponent("https://www.youtube.com/feeds/videos.xml?channel_id=");
 function loadVideo(iframe) {
 	$.getJSON(reqURL + iframe.getAttribute("cid"), function (data) {
 		var videoNumber = iframe.getAttribute("vnum")
 			? Number(iframe.getAttribute("vnum"))
 			: 0;
-		console.log(data.items.length - videoNumber);
-		console.log(data.items);
-		var link = data.items[data.items.length - videoNumber].link;
-		console.log(data.items[data.items.length - videoNumber].link)
+		console.log(videoNumber);
+		var link = data.items[videoNumber].link;
 		id = link.substr(link.indexOf("=") + 1);
 		iframe.setAttribute(
 			"src",
